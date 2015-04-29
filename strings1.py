d785c84f68ec1f7d5a7c6b4a140d111f5b1949ed
Robert Okun <robert@okun.com>
2015-04-29 11:09:02 -0400

    Questions remain but project complete in terms of code and cleanup

--- strings1.py
+++ strings1.py
@@ -1,12 +1,7 @@
 __author__ = 'robertokun'
 
 
-'''TODO:    1. Find out about GIT setup before project (Google Strings, etc)....if done correctly
-            2. Ask about Hot Links and VURB etc... are HOT LINKS really bad? Obviously dependant!
-            3. Divo Theme tag, can it be removed?
-            4. Copying Chinese language (Mandarin) with an audio button? Perhaps Animation as well?
-            5. Get theme transferred over to staging.okun.com and not use okun.com right now...
-            6. Monday schedule (1/20 web dev) (1/2 Python Google projects review) ? ok?
-            7. How does one manage those icons on banner top right of macbook screen?
-            8. How do I upload an image? Please lets execute on one.
+'''TODO:
+1. review the testing procedures outlined in the Google projects. This seems to be boiler plate throughout industry.
+2.
 '''
