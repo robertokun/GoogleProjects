030beaf0776b28f961ab3fe88780dfac9bfc82eb
Robert Okun <robert@okun.com>
2015-05-29 15:28:31 -0400

    removed unused files

--- strings1.py
+++
@@ -1,9 +0,0 @@
-__author__ = 'robertokun'
-
-
-'''TODO:
-   x1x. review the testing procedures outlined in the Google projects. This seems to be boiler plate throughout industry.
-2. go over the nose tests and other testing procedures on Learn Python the Hard Way
-3. figure out how or which text editor to work with for HTML/CSS files for website.
-   x4x. go over how to pull up sys code for built ins... for example see the actual code that allows replace to replace?
-'''
